8672359707070819dd04389c428819f6a0143aae
wsm <william.mcilwain@icloud.com>
2018-02-23 19:09:16 -0800

    Got basic assignment working

--- app.py
+++ app.py
@@ -34,11 +34,58 @@ def names():
     session = Session(engine)
 
     # Query samples
-    result=[column.key for column in Samples.__table__.columns]
+    results=[column.key for column in Samples.__table__.columns]
     session.close()
+    return jsonify(samples=results)
 
-    return render_template('index.html', names=result)
+@app.route('/samples/<sample>')
+def samples(sample):
+    
+    #/samples/<sample>
+    import os
 
+    import pandas as pd
+    import numpy as np
+
+    import sqlalchemy
+    from sqlalchemy.ext.automap import automap_base
+    from sqlalchemy.orm import Session
+    from sqlalchemy import create_engine
+    import sqlite3
+    import json
+
+    from flask import Flask, jsonify, render_template
+    app = Flask(__name__)
+
+    # DB connection setup
+    dbfile = os.path.join('db', 'data/belly_button_biodiversity.sqlite')
+    engine = create_engine("sqlite:///data/belly_button_biodiversity.sqlite")
+
+    # reflect an existing database into a new model
+    Base = automap_base()
+    # reflect the tables
+    Base.prepare(engine, reflect=True)
+
+    # Save references to each table
+    Samples_Metadata = Base.classes.samples_metadata
+    OTU = Base.classes.otu
+    Samples = Base.classes.samples
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    args=sample
+
+    sample_id=int(args[3:6])
+
+    qSamples = session.query(Samples.otu_id, getattr(Samples, args))
+    #print(qSamples[1])
+    dfSamples = pd.read_sql(qSamples.statement, qSamples.session.bind)
+    dfSamples.columns = ['otu_ids', 'sample_values']
+    dfSamples_sorted = dfSamples.sort_values('sample_values', axis=0, ascending=False)
+    dSamples = dfSamples_sorted.to_dict('list')
+    session.close()
+    return jsonify(samples=dSamples)
    
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/charts.js
@@ -0,0 +1,125 @@
+function init() {
+
+    var layout = {
+      height: 500,
+      width: 500
+    };
+
+    addSelector();
+    
+    initialSample="BB_940";
+    samplesURL="/samples/"+initialSample;
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", samplesURL, false);
+    xhttp.send();
+    
+    var myArray = JSON.parse(xhttp.responseText);
+    
+    var labels = myArray.samples.otu_ids.slice(0,9);
+    var data_points = myArray.samples.sample_values.slice(0,9);
+    
+    var data = [{
+        values: data_points,
+        labels: labels,
+        type: "pie"
+    }];   
+    
+    Plotly.plot("pie", data, layout);
+    
+    // Bubble chart
+    const xAxis = [];
+    const yAxis = [];
+    const radii = [];
+    const colors = [];
+    const UPPER_BOUND = 9;
+    // Create an x-axis
+    // Create corresponding y-data w/sizes correlated to x coordinate
+    // @Objective
+    
+    var red=0;
+    var green=0;
+    var blue=0;
+    
+    for (var x = 0; x < UPPER_BOUND; x++) {
+        xAxis.push(labels[x]);
+        yAxis.push(data_points[x]);
+
+        // TODO: Change the value below to experiment
+        radii.push(data_points[x]);
+        red=Number(labels[x]);
+        green=Number(labels[x]);
+        blue=Number(labels[x]);
+        colors.push(`rgb(${red/100},${green/1000},${blue/10}`);
+        }
+
+    // @Objective
+    const bubbledata = [{
+        x: xAxis,
+        y: yAxis,
+        mode: "markers",
+        marker: {
+            size: radii,
+            color: colors
+        }
+    }];
+
+    // @Objective
+    const bubblelayout = {
+        title: "Randomly Generated Data",
+        showlegend: false,
+        height: 500,
+        width: 1000
+        };
+
+    Plotly.newPlot("bubble", bubbledata, bubblelayout); 
+}
+
+function updatePlotly(newdata) {
+    var PIE = document.getElementById("pie");
+    Plotly.restyle(PIE, "values", [newdata[0].values]);
+    Plotly.restyle(PIE, "lables", [newdata[0].labels]);
+}
+
+function getData(dataset) {
+    var data = [];
+    
+    initialSample=dataset;
+    samplesURL="/samples/"+dataset;
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", samplesURL, false);
+    xhttp.send();
+    
+    var myArray = JSON.parse(xhttp.responseText);
+    
+    var labels = myArray.samples.otu_ids.slice(0,9);
+    var data_points = myArray.samples.sample_values.slice(0,9);
+
+    var data = [{
+        values: data_points,
+        labels: labels
+        }];
+    
+    updatePlotly(data);
+}
+
+function addSelector(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "/names", false);
+    xhttp.send();
+
+    if (xhttp.readyState==4 && xhttp.status==200)
+      {
+         var a = JSON.parse(xhttp.responseText);
+      }
+    var elem = document.getElementById('selDataset');
+    
+    for (i = 1; i < a.samples.length; i++) {
+        var element=a.samples[i];
+        elem.options.add( new Option(element, element) );
+    }
+    
+}
+
+init();
